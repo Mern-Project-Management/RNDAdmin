8ab3c0c939520d110c21b5698ae6418323b85dff
Prachibariya <prachibariya03@gmail.com>
2026-04-09 14:09:50 +0530

    update dist folder

--- backend/dist/assets/Navbar-ijVKnxrv-1775722581920.js
+++
@@ -1,1 +0,0 @@
-import{r as t,u as r,a as o,j as s}from"./index-Tjg3wZTa-1775722581920.js";import i from"./Navbar-ClNFHAgd-1775722581920.js";import"./Footer-CeSnaaqA-1775722581920.js";const p=()=>{const[c,l]=t.useState(!1),{data:e}=r(),{data:a}=o();return console.log(e),s.jsx("header",{className:"w-full",children:s.jsx(i,{categories:e,serviceCategories:a})})};export{p as default};
--- backend/dist/assets/introduction-B-qO1hgl-1775722581920.js
+++
@@ -1,1 +0,0 @@
-const n="/assets/introduction-CeE040n8-1775722581920.png";export{n as i};
