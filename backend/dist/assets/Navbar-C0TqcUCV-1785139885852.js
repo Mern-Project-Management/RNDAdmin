35184b19903d2c00975e08c4ef8507ba42a6b610
Prachibariya <prachibariya03@gmail.com>
2026-07-27 13:44:12 +0530

    update dist

---
+++ backend/dist/assets/Navbar-C0TqcUCV-1785139885852.js
@@ -0,0 +1,1 @@
+import{r as t,u as r,a as o,j as s}from"./index-B9Uf9VEF-1785139885852.js";import i from"./Navbar-CD0-SXPf-1785139885852.js";import"./Footer-CXHI73wl-1785139885852.js";const p=()=>{const[c,l]=t.useState(!1),{data:e}=r(),{data:a}=o();return console.log(e),s.jsx("header",{className:"w-full",children:s.jsx(i,{categories:e,serviceCategories:a})})};export{p as default};
--- backend/dist/assets/introduction-BhTU4En9-1785139192561.js
+++
@@ -1,1 +0,0 @@
-const n="/assets/introduction-CeE040n8-1785139192561.png";export{n as i};
